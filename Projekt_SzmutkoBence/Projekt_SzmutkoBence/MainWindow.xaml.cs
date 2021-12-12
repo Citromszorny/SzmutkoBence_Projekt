f0fed182fdb84cd7ac1231909908aef17316e591
Citromszorny <bence5605@gmail.com>
2021-12-12 16:38:34 +0100

    2-3 feladat egyben
    De a 1/c az biztos, meg az 1/d is meg a kijelölt visszaállítása meg a végleges törlése. Elfelejtettem menteni 😬

--- Projekt_SzmutkoBence/Projekt_SzmutkoBence/MainWindow.xaml.cs
+++ Projekt_SzmutkoBence/Projekt_SzmutkoBence/MainWindow.xaml.cs
@@ -32,14 +32,25 @@ namespace Projekt_SzmutkoBence
             check.Unchecked += new RoutedEventHandler(checkbox1_Unchecked_1);
             check.Content = feldatSzoveg.Text;
             feladatokListaja.Items.Add(check);
+            feldatSzoveg.Clear();
             
         }
-
+        private void feladatokListaja_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CheckBox kijelolKiirasa = (CheckBox)feladatokListaja.SelectedItem;
+            if (kijelolKiirasa == null)
+            {
+                return;
+            }
+            feldatSzoveg.Text = kijelolKiirasa.Content.ToString();
+        }
         private void kijeloltTorlese_Click(object sender, RoutedEventArgs e)
         {
             CheckBox nev2 = (CheckBox)feladatokListaja.SelectedItem;
             feladatokListaja.Items.Remove(nev2);
-            toroltElemek.Items.Add(nev2.Content);
+            toroltElemek.Items.Add(nev2);
+            feladatokListaja.Items.Refresh();
+            toroltElemek.Items.Refresh();
         }
         private void checkbox_Checked(object sender, RoutedEventArgs e)
         {
@@ -55,10 +66,23 @@ namespace Projekt_SzmutkoBence
             nev.FontStyle = FontStyles.Normal;
             nev.Foreground = Brushes.Black;
         }
-
-        private void checkbox12_Click(object sender, RoutedEventArgs e)
+        private void kijeloltVisszaAll_Click(object sender, RoutedEventArgs e)
         {
-        
+            CheckBox kijeloltVissza = (CheckBox)toroltElemek.SelectedItem;
+            toroltElemek.Items.Remove(kijeloltVissza);
+            feladatokListaja.Items.Add(kijeloltVissza);
+        }
+
+        private void kijeloltVegTorlese_Click(object sender, RoutedEventArgs e)
+        {
+            CheckBox torles = (CheckBox)toroltElemek.SelectedItem;
+            toroltElemek.Items.Remove(torles);
+        }
+
+        private void kijeloltModositas_Click(object sender, RoutedEventArgs e)
+        {
+            CheckBox modosit = (CheckBox)feladatokListaja.SelectedItem;
+            modosit.Content = feldatSzoveg.Text;
         }
     }
 }
